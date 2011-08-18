9af1a26d82fad9745702e8067775d8aa95559f46
retopara <xqqixp@gmail.com>
2011-08-18 17:09:55 +0800

    modified da_sizeconsist.pl

--- script/da_sizeconsist.pl
+++ script/da_sizeconsist.pl
@@ -8,13 +8,25 @@ my $inputf;
 open ($inputf, "< outdata.txt") or die "Could not open file outdata.txt: $!";
 while (my $line = <$inputf>)
 {
+	if ($line =~ /^NS/)
+	{
+		my @cont_ns = split (/\s+/, $line);
+		print "$line";
+	}
+
+	if ($line =~ /^VD/)
+	{
+		my @cont_vd = split (/\s+/, $line);
+		print "$line";
+	}
+
 	if ($line =~ /^groud/)
 	{
 		my @cont_gse = split (/\s+/, $line);
-    print "$line\n"
+    print "$line";
 #    my $num = scalar(@cont_gse);
-		my $e = $cont_gse[4];
-    print "$e\n";
+#    my $e = $cont_gse[4];
+#    print "$e\n";
 		
 	}
 }
